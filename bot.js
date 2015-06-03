d1e79d9a93ad65d95b02812afbb7215b1e578a83
Licson Lee <licson0729@gmail.com>
2015-06-03 21:50:31 +0800

    Fixed file saving

--- bot.js
+++ bot.js
@@ -48,8 +48,8 @@ Bot.prototype.setupEventHandlers = function(){
 	process.stdin.resume();
 	process.nextTick(function() {
 		process.on('exit', function(){
-			fs.writeFileSync(__dirname + '/admins.json', JSON.stringify(self.admins));
-			fs.writeFileSync(__dirname + '/banlist.json', JSON.stringify(self.banlist));
+			fs.writeFileSync(__dirname + '/components/admins.json', JSON.stringify(self.admins));
+			fs.writeFileSync(__dirname + '/components/banlist.json', JSON.stringify(self.banlist));
 			// fs.writeFileSync(__dirname + '/options.json', JSON.stringify(self.opts));
 		});
 		
