27609fd0f94fe9e25add2cc6c80415481254611c
Licson Lee <licson0729@gmail.com>
2015-09-29 19:20:48 +0800

    Supports IPv6 and display more information for IP lookup

--- components/netlookup.js
+++ components/netlookup.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var IP_ADDRESS = /^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$/;
+var IP6_ADDRESS = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/;
 var cache = {};
 
 // Clear cache every hour
@@ -18,7 +19,7 @@ module.exports = [
 		def: function(args, target){
 			var self = this;
 
-			if(!IP_ADDRESS.test(args[0])){
+			if(!IP_ADDRESS.test(args[0]) || !IP6_ADDRESS.test(args[0])){
 				this.say(target, 'Please specify an IP address.');
 				return;
 			}
@@ -34,6 +35,18 @@ module.exports = [
 
 					if(info.city != null){
 						output += ' | City: ' + info.city;
+					}
+					
+					if(info.region != null){
+						output += ' | Region: ' + info.region;
+					}
+					
+					if(info.hosting){
+						output += '\nThis IP belongs to a hosting company.';
+					}
+					
+					if(info.carrier){
+						output += '\nThis IP belongs to a mobile carrier named "' + info.carrier + '"';
 					}
 
 					self.say(target, output);
